c8f441e5fcaf6f5f8607d71d0b40376cb7e0c205
Neha Bisht <neha.bisht@tothenew.com>
2023-12-06 12:06:23 +0530

    laying foundation part-2

--- App.js
+++ App.js
@@ -1,34 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// createElement approach is not a good way as it looks complex.
-// That's why we use JSX instead of createElement.
+const num = 10000;
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Namastey React 🚀"
-);
-const jsxheading = (
+const elem = <span>React Element</span>;
+
+const Title = () => {
+  return (
+    <h1 className="head" tabIndex="4">
+      Hey
+    </h1>
+  );
+};
+
+// fn1 & fn2 will return true as they are same
+
+// React functional component
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      {num + 500}
+
+      <Title />
+      <h3>Namastey React using functional component 🚀 </h3>
+    </div>
+  );
+};
+
+// React element
+const head = (
   <h1 id="heading" className="head" tabname="5">
-    Namastey React using JSX 🚀
+    {/* Namastey React using JSX 🚀 Namaste React */}
+    {elem}
+    {Title()}
+    <HeadingComponent />
   </h1>
 );
 
-console.log("js", jsxheading);
-
-//JSX is not HTML in JS. It has HTML-like or XML-like syntax
-// JSX is transpiled before it reaches the JS engine - PARCEL
-
-// As soon as we write JSX, Parcel creates bable which
-//  quickly converts this code in such a way that will be understood by React.
-// Babel converts the JSX into code that is understood by browser/React/JS engine
-
-// JSX => React.createElement - JS object => HtmlElement(render)
-
-//JSX code is transpiled into React.createElement. React.createElement is a JS object which is
-//  rendered onto the DOM as HTML element. Babel converts JSX code into React.createElement
-// Visit Babel.js
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxheading);
+root.render(head);
+
+// Difference between Element & component
+// Component composition -> Component inside component
+// Inside JSX when we write curly braces then inside curly braces {} we can run any piece of JS code.
+
+const fn1 = () => {
+  return true;
+};
+
+const fn2 = () => true; // shorthand syntax
